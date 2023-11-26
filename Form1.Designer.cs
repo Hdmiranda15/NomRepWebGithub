43e13d5c98bb855e8c13d1a37610d162039f5d22
Henry Miranda <hdmirandaingelec15@gmail.com>
2023-11-26 14:34:20 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace U4_memoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPU = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.hhhh = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.gbxEntrada = new System.Windows.Forms.GroupBox();
+            this.cbxProducto = new System.Windows.Forms.ComboBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.gbxTotal = new System.Windows.Forms.GroupBox();
+            this.lblIVA = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.Salir = new System.Windows.Forms.Button();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.gbxEntrada.SuspendLayout();
+            this.gbxTotal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Seleccion Producto";
+            // 
+            // txtPU
+            // 
+            this.txtPU.Location = new System.Drawing.Point(127, 41);
+            this.txtPU.Name = "txtPU";
+            this.txtPU.Size = new System.Drawing.Size(100, 20);
+            this.txtPU.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 44);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Precio Unitario(Sin IVA)";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(126, 65);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 12;
+            // 
+            // hhhh
+            // 
+            this.hhhh.AutoSize = true;
+            this.hhhh.Location = new System.Drawing.Point(23, 68);
+            this.hhhh.Name = "hhhh";
+            this.hhhh.Size = new System.Drawing.Size(49, 13);
+            this.hhhh.TabIndex = 13;
+            this.hhhh.Text = "Cantidad";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(240, 18);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(79, 63);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // gbxEntrada
+            // 
+            this.gbxEntrada.Controls.Add(this.cbxProducto);
+            this.gbxEntrada.Controls.Add(this.btnAdd);
+            this.gbxEntrada.Controls.Add(this.hhhh);
+            this.gbxEntrada.Controls.Add(this.txtCantidad);
+            this.gbxEntrada.Controls.Add(this.label2);
+            this.gbxEntrada.Controls.Add(this.txtPU);
+            this.gbxEntrada.Controls.Add(this.label1);
+            this.gbxEntrada.Location = new System.Drawing.Point(12, 12);
+            this.gbxEntrada.Name = "gbxEntrada";
+            this.gbxEntrada.Size = new System.Drawing.Size(342, 91);
+            this.gbxEntrada.TabIndex = 5;
+            this.gbxEntrada.TabStop = false;
+            this.gbxEntrada.Text = "Ingreso de Producto(memoria ESTATICA)";
+            // 
+            // cbxProducto
+            // 
+            this.cbxProducto.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cbxProducto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxProducto.FormattingEnabled = true;
+            this.cbxProducto.Items.AddRange(new object[] {
+            "Monitor",
+            "Teclado",
+            "Mouse",
+            "Otros"});
+            this.cbxProducto.Location = new System.Drawing.Point(126, 16);
+            this.cbxProducto.Name = "cbxProducto";
+            this.cbxProducto.Size = new System.Drawing.Size(100, 21);
+            this.cbxProducto.TabIndex = 18;
+            this.cbxProducto.SelectedIndexChanged += new System.EventHandler(this.cbxProducto_SelectedIndexChanged);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(46, 65);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(157, 23);
+            this.btnMostrar.TabIndex = 15;
+            this.btnMostrar.Text = "Mostrar Total";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // gbxTotal
+            // 
+            this.gbxTotal.Controls.Add(this.lblIVA);
+            this.gbxTotal.Controls.Add(this.lblTotal);
+            this.gbxTotal.Controls.Add(this.btnMostrar);
+            this.gbxTotal.Controls.Add(this.label4);
+            this.gbxTotal.Controls.Add(this.label5);
+            this.gbxTotal.Location = new System.Drawing.Point(366, 12);
+            this.gbxTotal.Name = "gbxTotal";
+            this.gbxTotal.Size = new System.Drawing.Size(266, 91);
+            this.gbxTotal.TabIndex = 15;
+            this.gbxTotal.TabStop = false;
+            this.gbxTotal.Text = "Total";
+            // 
+            // lblIVA
+            // 
+            this.lblIVA.AutoSize = true;
+            this.lblIVA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblIVA.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.lblIVA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIVA.ForeColor = System.Drawing.Color.Red;
+            this.lblIVA.Location = new System.Drawing.Point(153, 39);
+            this.lblIVA.Name = "lblIVA";
+            this.lblIVA.Size = new System.Drawing.Size(29, 15);
+            this.lblIVA.TabIndex = 17;
+            this.lblIVA.Text = "IVA";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotal.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblTotal.Location = new System.Drawing.Point(142, 14);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(86, 15);
+            this.lblTotal.TabIndex = 16;
+            this.lblTotal.Text = "Total a Pagar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "IVA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Total a Pagar +IVA";
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(12, 109);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(599, 234);
+            this.dgvProductos.TabIndex = 17;
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(701, 282);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(88, 24);
+            this.Salir.TabIndex = 4;
+            this.Salir.Text = "salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.salir_Click);
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Location = new System.Drawing.Point(701, 251);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(88, 25);
+            this.Limpiar.TabIndex = 3;
+            this.Limpiar.Text = "limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            this.Limpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 645);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.gbxTotal);
+            this.Controls.Add(this.gbxEntrada);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Limpiar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxEntrada.ResumeLayout(false);
+            this.gbxEntrada.PerformLayout();
+            this.gbxTotal.ResumeLayout(false);
+            this.gbxTotal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPU;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label hhhh;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox gbxEntrada;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.GroupBox gbxTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.ComboBox cbxProducto;
+        private System.Windows.Forms.Label lblIVA;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button Limpiar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+/* memoria estatica = Tamaño[k] + Asignacion antes Mestatica(compilacion,Tiempode ejeucion)
+
+   Variables Estáticas:  int[] numeros = new int[10];
+          TipoDedDato CrearArray NombreVar = InstanciaObj TipoDatos[AsignacionDeMemoria]
+            
+                           static int contador = 0;
+              MiembrodeClaseEstatico TipoDato NomVar = 0;
+
+    Métodos Estáticos:  public static void metodoEstatico() {  operación estática } 
+         AccesibleGeneral PerteneceAlaClase NoRetornaValor NombMetodo()             () != Argmentos
+
+    Constantes Estáticas:   public static class Constantes {  public const double PI = 3.14159265359;  }
+  AccesibleGeneral PerteneceAlaClase CreandoClase NomClas {  AccesibleGeneral Constante TipoDato NomVar} 
+
+    como formula "public static class V : public const double N = V.N
+ */
+
+/* memoria dinamica  = puede agregar o elimitar DATOS (tiempos de Ejecucion) , administra el desarrollador
+
+   
+ */
+ 
+
+
+namespace U4_memoria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ///Es la inicializacion del DataGrid para mostrar los encabezados dentro de la Grid
+            dgvProductos.Columns.Add("NombreProducto", "Producto");
+            dgvProductos.Columns.Add("PrecioUnitario", "Precio");
+            dgvProductos.Columns.Add("Cantidad", "Cantidad");
+            dgvProductos.Columns.Add("SubTotal", "Sub Total");
+
+        }
+        //////////////////////////////////////////NIVEL DE CLASE //////////////////////////////////////////
+
+        /// <summary>
+        /// Declaración de variables a nivel de Clase, nótese que no están dentro de método clic
+        /// están al mismo nivel por ello este tipo de declaración es conocida a nivel de Clase
+        /// </summary>
+        const double IVA = 0.13; // Memoria estática inmutable
+        const int LIMITE = 5;   // Memoria estática inmutable
+        int item = 0;           // Memoria estática mutable
+        /// <summary>
+        /// se han declarado arreglos del mismo tipo donde se utilizan las constantes del límite que se tendrán
+        /// la variable item es la que servirá en el modelo para saber la posición del elemento
+        /// </summary>
+        int[] cantidad = new int[LIMITE];
+        double[] precio = new double[LIMITE];
+        string[] producto = new string[LIMITE];
+        //////////////////////////////////////////NIVEL DE CLASE //////////////////////////////////////////
+
+        private void cbxProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Tu código para manejar el evento SelectedIndexChanged
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Tu código para manejar el evento Load del formulario
+        }
+
+        private void salir_Click(object sender, EventArgs e)
+        {
+            //Application.Exit();// cerrar la aplicacion
+            //Class.metodo
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            //dgvProductos.Rows.Clear(); // Elimina todas las filas del DataGridView.
+
+            //txtPU.Text = ""; // Establece el contenido del TextBox en blanco.
+           // txtCantidad.Text = ""; // Establece el contenido del TextBox en blanco.
+            
+           // lblTotal.Text = ""; // Establece el contenido de la etiqueta en blanco.
+           // lblIVA.Text = ""; // Establece el contenido de la etiqueta en blanco.
+
+            ////////////////////////////////////////// MEMORIA estatica //////////////////////////////////////////
+            ///////////////////////////////////////////// MEMORIA estatica //////////////////////////////////////////
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ///se comienza con inicializaciones y validaciones de entrada
+            bool esNumero = false;//establenciendo La Variable False
+
+            if (item >= LIMITE)//cuando se supere el LIMITE
+            {
+                MessageBox.Show("Sobrepasa el limite permitido");
+                return;
+            }
+            producto[item] = cbxProducto.SelectedItem.ToString();//toma el Valor Seleccionado en el cuadro de producto
+                                                                 //lo convierte a texto y lo almacena en la matriz producto en el sub-indice (ITEM)
+
+            esNumero = double.TryParse(txtPU.Text, out precio[item]);// extrae del cuadro el numero en forma de Texto y lo convierte a Double y almacena
+                                                                     //  en Matriz Precio en el sub-indice ITEM , si es correcto , esNumero permanece FALSE
+            if (!esNumero)
+            {
+                MessageBox.Show("Ingrese un Precio Valido");
+                return;
+            }
+            esNumero = int.TryParse(txtCantidad.Text, out cantidad[item]);// extrae del cuadro el numero en forma de Texto y lo convierte a Double y almacena
+                                                                          //  en Matriz cantidad en el sub-indice ITEM , si es correcto , esNumero permanece FALSE
+            if (!esNumero)
+            {
+                MessageBox.Show("Ingrese una Cantidad Valida");
+                return;
+            }
+            //una vez guardada la información aumentamos en uno para tener la siguiente posición del elemento
+             item++;
+            MessageBox.Show("Producto Agregado Satistactoriamente");
+        }
+        ////////////////////////////////////////// MEMORIA estatica //////////////////////////////////////////
+        ///////////////////////////////////////////// MEMORIA estatica //////////////////////////////////////////
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            ///Esta parte se hizo con la finalidad de demostrar cómo se recorre un arreglo
+            ///se inicializarán las variables resumen para evitar que almacene basura
+            double total = 0;
+            double totalIVA = 0;
+            ///Limpiar los elementos del datagrid
+            dgvProductos.Rows.Clear();
+            for (int elemento = 0; elemento < LIMITE; elemento++)
+            {
+                double subTotal = 0;
+                subTotal = precio[elemento] * cantidad[elemento];
+                total += subTotal;
+                ///Agrega el elemento dentro del datagrid para que sean mostrados
+                dgvProductos.Rows.Add(producto[elemento], precio[elemento],cantidad[elemento], subTotal);
+            }
+            ///una vez recorrido todos los elementos se calcula el IVA
+            totalIVA = total * IVA;
+            ///Después de calculado el IVA se usa la misma variable para que el total se le agregue el IVA
+            total = total + totalIVA;
+            lblTotal.Text = total.ToString();//incorporando al LABEL en texto,la variable convertida en String 
+            lblIVA.Text = totalIVA.ToString();//incorporando al LABEL en texto,la variable convertida en String 
+        }       
+    }
+}
+ 
