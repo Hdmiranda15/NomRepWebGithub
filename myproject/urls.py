4f776fe154eb7232bd1aad823db8b7d673870a0d
Hdmiranda15 <113644636+Hdmiranda15@users.noreply.github.com>
2025-06-10 17:31:29 -0600

    Update urls.py
    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,30 +1,22 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# myproject/urls.py
 from django.contrib import admin
-from django.urls import path, include # Ensure include is imported
-from django.conf import settings # Import settings
-from django.conf.urls.static import static # Import static
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView # Importar esta clase para la redirección
 
 urlpatterns = [
+    # Redirige la URL raíz (vacía) a la vista del dashboard del primer empleado
+    # Esto resolverá el 404 en http://0.0.0.0:8000/
+    path('', RedirectView.as_view(url='/primerempleado/', permanent=False)),
+
     path('admin/', admin.site.urls),
-    # Remove the old 'gestion/' prefix if it exists, and include directly
+    # Incluye las URLs de tu aplicación gestion_impresion.
+    # Como gestion_impresion/urls.py ya tiene 'primerempleado/' y 'segundoempleado/',
+    # al incluirlo con path('', include(...)), se accede directamente a /primerempleado/ y /segundoempleado/
     path('', include('gestion_impresion.urls')),
 ]
 
-# Serve media files during development
+# Sirve los archivos de medios (subidos por usuarios) durante el desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
